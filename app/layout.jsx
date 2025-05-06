2d3ed514bcdc6f95a1e269e7f962f09cc913b9f7
David Pam <niratpam@gmail.com>
2025-05-07 00:30:04 +0100

    Home Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,13 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                {children}
+                <header>
+                  [header]
+                </header>
+                <main>{children}</main>
+                <footer>
+                  [footer]
+                </footer>
             </body>
         </html>
     )
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Indie Gamer</h1>
+      <p>Welcome to the Indie Gamer community!</p> 
+    </>
+  )
+}
