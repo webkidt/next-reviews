d6da16820b9e71af55b147539890052c79a85360
David Pam <niratpam@gmail.com>
2025-05-07 01:31:37 +0100

    Page Routes

---
+++ app/reviews/page.jsx
@@ -0,0 +1,8 @@
+export default function ReviewPage() {
+  return (
+    <>
+      <h1>Reviews</h1>
+      <p>Here we'll list all the reviews.</p>
+    </>
+  )
+}
---
+++ app/reviews/stardew-valley/page.jsx
@@ -0,0 +1,10 @@
+const StardewValleyPage = () => {
+  return (
+    <>
+      <h1>Stardew Valley</h1>
+      <p>This will be the review for Stardew Valley.</p>
+    </>
+  );
+};
+
+export default StardewValleyPage;
